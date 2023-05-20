6181b775d5135bec98b73df119fe689ce4103b37
Obi Obianom <oobianom@umaryland.edu>
2023-05-19 21:14:20 -0400

    Update toolkit

--- R/engine.R
+++ R/engine.R
@@ -5,19 +5,18 @@
 #' @export
 
 
-
-
 engine <- function() {
-  R6::R6Class('Engine',
-              public = list(
-                views = 0,
-                likes = 0,
-                comments = 0
-              ),
-              private = list(
-                API = NULL,
-                User = NULL,
-                Name = NULL,
-                IP = NULL
-              ))
+  R6::R6Class("Engine",
+    public = list(
+      views = 0,
+      likes = 0,
+      comments = 0
+    ),
+    private = list(
+      API = NULL,
+      User = NULL,
+      Name = NULL,
+      IP = NULL
+    )
+  )
 }
