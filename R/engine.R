a4e792850b9f9b2274a98d54686f4e20414a9d8b
Obi Obianom <oobianom@umaryland.edu>
2023-05-20 01:20:06 -0400

    Update engine.R

--- R/engine.R
+++ R/engine.R
@@ -5,12 +5,20 @@
 #' @export
 
 
-engine <- function() {
+engine <- function(APIk) {
   R6::R6Class("Engine",
     public = list(
       views = 0,
       likes = 0,
-      comments = 0
+      comments = 0,
+      initialize = function(API = APIk) {
+        self$views = self$views + 1
+        private$API = API
+      },
+      print = function(...) {
+        print(...)
+        invisible(self)
+      }
     ),
     private = list(
       API = NULL,
