5cfda72be876e29d4d0603c27b3a60e58b9bac1d
Obi Obianom <oobianom@umaryland.edu>
2023-05-20 04:25:51 -0400

    R6 refine

--- R/engine.R
+++ R/engine.R
@@ -7,21 +7,17 @@
 
 engine <- function(API) {
   R6::R6Class("Engine",
-    lock_class = TRUE,
+    #lock_class = TRUE,
     public = list(
       views = 0,
       likes = 0,
       comments = 0,
-      initialize = function(APIKEY = API) {
-        self$views <- self$views + 1
-        private$API <- APIKEY
-      },
       print = function(...) {
         invisible(self)
       }
     ),
     private = list(
-      API = NULL,
+      API = "hot",
       User = NULL,
       Name = NULL,
       IP = NULL
--- R/setup.R
+++ R/setup.R
@@ -8,24 +8,23 @@
 
 
 setup <- function(API) {
-  registerAPI <- engine(API)
   R6::R6Class("SetupKit",
-              inherit = registerAPI,
-              public = list(
-                initialize = function() {
-                  print("...starting")
-                },
-                print = function(...) {
-                  invisible(self)
-                },
-                getApi = function(){
-                  print(private$API)
-                }
-
-              ),
-              private = list(
-                temp = NULL
-              )
+    inherit = engine(),
+    public = list(
+      initialize = function(APIKEY = API) {
+        self$views <- self$views + 1
+        private$API <- APIKEY
+        print("...starting")
+      },
+      print = function(...) {
+        invisible(self)
+      },
+      getApi = function() {
+        print(private$API)
+      }
+    ),
+    private = list(
+      temp = NULL
+    )
   )
-
 }
